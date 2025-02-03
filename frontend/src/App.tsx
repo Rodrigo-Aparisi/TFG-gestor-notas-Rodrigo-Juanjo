0dc03888d01647d7c0610d215a08ec94e6d28779
Juanjo <juanjo.munozgil@gmail.com>
2025-02-03 22:29:08 +0100

    Pagina de configuracion, css y codigo correspondiente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/index.ts';
+import { store } from './store/index';
 import Header from './components/Layout/Header.tsx';
 import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
 import Notes from './pages/Notes.tsx';
 import Account from './pages/account.tsx';
-import PrivateRoute from './components/PrivateRoute.tsx';
-import './App.css';
+import Settings from './pages/settings.tsx';
 import Calendar from './pages/Calendar.tsx';
+import PrivateRoute from './components/PrivateRoute';
+import './App.css';
 
 function App() {
   return (
@@ -21,24 +22,18 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
-              <Route 
-                path="/notes" 
-                element={
-                  <PrivateRoute>
-                    <Notes />
-                  </PrivateRoute>
-                } 
-              />
-              <Route 
-                path="/calendar" 
-                element={
-                  <PrivateRoute>
-                    <Calendar />
-                  </PrivateRoute>
-                } 
-              />
-              <Route path="/account" element={<Account />} /> 
-              <Route path= "/notes" element={<Notes />}/>
+              <Route path="/notes" element={
+                <PrivateRoute>
+                  <Notes />
+                </PrivateRoute>
+              } />
+              <Route path="/calendar" element={
+                <PrivateRoute>
+                  <Calendar />
+                </PrivateRoute>
+              } />
+              <Route path="/account" element={<Account />} />
+              <Route path="/settings/*" element={<Settings />} />
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
