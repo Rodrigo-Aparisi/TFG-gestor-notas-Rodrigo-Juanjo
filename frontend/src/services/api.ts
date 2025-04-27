dc3a7972d336865df7c8aeb94044890250b620bb
Juanjo <juanjo.munozgil@gmail.com>
2025-04-27 18:22:50 +0200

    Funcion de compartir notas con usuarios hecha, falta la posibilidad de editar por los otros usuarios

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -77,6 +77,16 @@ export const noteService = {
     return response.data;
   },
 
+  getSharedNotes: async () => {
+    try {
+      const response = await api.get('/notes/shared-notes');
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching shared notes:', error);
+      throw error;
+    }
+  },
+
   updateNote: async (id: string, noteData: { title?: string; content?: string }) => {
     try {
       const sanitizedData = {
