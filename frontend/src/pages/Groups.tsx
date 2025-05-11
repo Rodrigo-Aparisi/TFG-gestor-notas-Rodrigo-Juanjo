01b92750e32ab460568be9036b4bd7d17fbc69d1
Juanjo <juanjo.munozgil@gmail.com>
2025-05-11 14:34:36 +0200

    Inicios de grupos de personas con notas

---
+++ frontend/src/pages/Groups.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import '.../styles/groups.css';
+
+const Groups: React.FC = () => {
+    return (
+        <div className="groups-container">
+            <h1>Grupos</h1>
+        </div>
+    )
+
+};
+
+export default Groups;
